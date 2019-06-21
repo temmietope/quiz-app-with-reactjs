04a6d317700c706386881bba585a2522f6bebd69
Temitope <temmieayodele@gmail.com>
2019-06-21 13:04:25 +0100

    added new features

--- src/Component/QuestionsAndOptions.js
+++ src/Component/QuestionsAndOptions.js
@@ -3,42 +3,66 @@ import { questions } from "../questions";
 
 class QuestionsAndOptions extends Component {
   state = {
-    questions: ""
+    questions: [],
+    currentQuestion: 0,
+    count: 0
   };
   componentDidMount() {
-    this.setState({ questions: questions });
+    this.setState({
+      questions
+    });
   }
-  loadQuestion = () => {
-    let index = 0;
-    const q = this.state.questions[index];
-    console.log(q.question);
-    console.log(q.options);
-    return <span>{q.question}</span>;
+  renderQuestion() {
+    const { questions, currentQuestion } = this.state;
+    return (
+      <span>
+        <div>
+          <p> {questions[currentQuestion].question}</p>
+        </div>
+        <div>
+          {questions[currentQuestion].options.map((option, index) => {
+            return (
+              <label key={index}>
+                <input
+                  type="radio"
+                  name="option"
+                  onClick={() => {
+                    this.selectedOption({ index });
+                  }}
+                />
+                {option}
+              </label>
+            );
+          })}
+        </div>
+      </span>
+    );
+  }
+  selectedOption = option => {
+    const { currentQuestion } = this.state;
+    if (questions[currentQuestion].answer === option.index) {
+      console.log("fine");
+    } else {
+      console.log("clown");
+    }
   };
+  nextQuestion = () => {
+    let { currentQuestion } = this.state;
+    this.setState({ currentQuestion: currentQuestion + 1 });
+    console.log(currentQuestion);
+  };
+
   render() {
     return (
-      <div className="wrapper">
-        <div className="exactQuestion" ref="exactQuestion">
-          {this.state.questions.length > 0 && this.loadQuestion()}
-        </div>
-        <div className="options">
-          <label className="option">
-            <input type="radio" name="option" value="1" />
-            <span id="opt1" />
-          </label>
-          <label className="option">
-            <input type="radio" name="option" value="2" />
-            <span id="opt2" />
-          </label>
-          <label className="option">
-            <input type="radio" name="option" value="3" />
-            <span id="opt3" />
-          </label>
-          <label className="option">
-            <input type="radio" name="option" value="4" />
-            <span id="opt4" />
-          </label>
-        </div>
+      <div>
+        {this.state.questions.length > 0 && this.renderQuestion()}
+        <button
+          onClick={() => {
+            this.nextQuestion();
+          }}
+        >
+          Next
+        </button>
       </div>
     );
   }
